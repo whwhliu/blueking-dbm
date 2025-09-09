139d5695d4b7cd4573b2367428923cee3a52571f
yksitu <1297650644@qq.com>
2025-09-09 10:47:35 +0800

    feat(mysql): 优化退回主机框架；开发集群部署对应的退回主机流程 #12825

--- dbm-ui/backend/flow/engine/controller/mysql.py
+++ dbm-ui/backend/flow/engine/controller/mysql.py
@@ -67,6 +67,7 @@ from backend.flow.engine.bamboo.scene.mysql.mysql_upgrade import MySQLStorageLoc
 from backend.flow.engine.bamboo.scene.mysql.pt_table_sync import PtTableSyncFlow
 from backend.flow.engine.bamboo.scene.mysql.validate.mysql_local_upgrade_validator import MySQLLocalUpgradeValidator
 from backend.flow.engine.bamboo.scene.mysql.validate.mysql_proxy_add_validator import MySQLProxyClusterAddFlowValidator
+from backend.flow.engine.bamboo.scene.mysql.revoke.mysql_single_apply_revoke_flow import MySQLSingleApplyRevokeFlow
 from backend.flow.engine.bamboo.scene.mysql.validate.mysql_proxy_reduce_validator import (
     MySQLProxyClusterReduceFlowValidator,
 )
@@ -78,6 +79,7 @@ from backend.flow.engine.bamboo.scene.mysql.validate.mysql_proxy_switch_for_migr
 )
 from backend.flow.engine.bamboo.scene.mysql.validate.mysql_proxy_switch_validator import MySQLProxySwitchValidator
 from backend.flow.engine.controller.base import BaseController
+from backend.flow.engine.revoke.base import revoke_with
 from backend.flow.engine.validate.base_validate import validates_with
 
 
@@ -86,6 +88,7 @@ class MySQLController(BaseController):
     mysql实例相关调用
     """
 
+    @revoke_with(MySQLSingleApplyRevokeFlow)
     def mysql_single_apply_scene(self):
         """
         部署tenDB(mysql)单实例场景(新flow编排)
